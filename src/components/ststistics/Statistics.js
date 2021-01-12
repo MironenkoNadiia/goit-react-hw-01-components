918187fef3e68089efcec5cd3b29b2c13cf3458d
MironenkoNadiia <candleflame@i.ua>
2021-01-12 20:53:45 +0200

    prettier

--- src/components/ststistics/Statistics.js
+++ src/components/ststistics/Statistics.js
@@ -1,61 +1,57 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const Section = styled.section`
-background-color:grey;
-max-width:700px
+  background-color: grey;
+  max-width: 700px;
 `;
 
 const StatList = styled.ul`
-list-style:none;
-display:flex;
-justify-content:center;
+  list-style: none;
+  display: flex;
+  justify-content: center;
 `;
 
 const StatItem = styled.li`
-display:flex;
-flex-direction:column;
-margin:30px
+  display: flex;
+  flex-direction: column;
+  margin: 30px;
 `;
 
 const Label = styled.span`
-color:white;
+  color: white;
 `;
 
 const Percentage = styled.span`
-color:white;
+  color: white;
 `;
 
 const Title = styled.h2`
-margin-left:300px ;
-padding-top:50px
+  margin-left: 300px;
+  padding-top: 50px;
 `;
 
-
-
 function Statistics({ title, stats }) {
-    return (
-        <Section>
-            {title && <Title>{title}</Title>}
+  return (
+    <Section>
+      {title && <Title>{title}</Title>}
 
-            <StatList>
-                {stats.map(stat => 
-                    <StatItem key={stat.id}>
-                            <Label>{stat.label}</Label>
-                            <Percentage>{stat.percentage}%</Percentage>
-                        </StatItem>
-                    
-                )}
-            </StatList>
-        </Section>
-    );
+      <StatList>
+        {stats.map(stat => (
+          <StatItem key={stat.id}>
+            <Label>{stat.label}</Label>
+            <Percentage>{stat.percentage}%</Percentage>
+          </StatItem>
+        ))}
+      </StatList>
+    </Section>
+  );
 }
 
 Statistics.propTypes = {
   title: PropTypes.string.isRequired,
   stats: PropTypes.array.isRequired,
 };
-
 
 export default Statistics;
